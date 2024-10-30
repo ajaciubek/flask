46222f93eb558a69e80b93b2a8e90225b0e0309b
Arkadiusz Jaciubek <arkadiusz.jaciubek@aptiv.com>
2024-10-30 13:30:07 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/calculator')
+def calculator():
+    req_args = request.args.to_dict()
+    res = f"{req_args['arg1']} + {req_args['arg2']} = {int(req_args['arg1']) + int(req_args['arg1'])}"
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=True)
