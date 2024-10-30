55a42376f2f99c196f7cf75937500375be566c41
Arkadiusz Jaciubek <arkadiusz.jaciubek@aptiv.com>
2024-10-30 13:37:07 +0100

    small fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/calculator')
 def calculator():
     req_args = request.args.to_dict()
-    res = f"{req_args['arg1']} + {req_args['arg2']} = {int(req_args['arg1']) + int(req_args['arg1'])}"
+    res = f"{req_args['arg1']} + {req_args['arg2']} = {int(req_args['arg1']) + int(req_args['arg2'])}"
     return res
 
 if __name__ == '__main__':
