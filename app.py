1588f5c218837501ef54cda85f42bb93cb5f8c33
jaciubek <jaciubek@d17-327d-pc-17.ipa.ii.agh.edu.pl>
2024-12-01 11:18:19 +0100

    Fix pylint

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request # pylint: disable=E0401
 
+"""
+App for calculator
+"""
+
 app = Flask(__name__)
 
 @app.route('/calculator')
 def calculator():
+    """
+    Main function for app
+    """
     req_args = request.args.to_dict()
-    res = f"{req_args['arg1']} + {req_args['arg2']} = {int(req_args['arg1']) + int(req_args['arg2'])}"
+    result = int(req_args['arg1']) + int(req_args['arg2'])
+    res = f"{req_args['arg1']} + {req_args['arg2']} = {result}"
     return res
 
 if __name__ == '__main__':
