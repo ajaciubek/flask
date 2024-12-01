74bcce3509d5e956673bff4f038ab90f610cfd39
jaciubek <jaciubek@d17-327d-pc-17.ipa.ii.agh.edu.pl>
2024-12-01 11:46:57 +0100

    Fix tests

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
-class Calculator(object):
+# pylint: disable=C0114
+# pylint: disable=C0115
+
+class Calculator():
     def __init__(self, x: float, y: float):
         self.__x = x
         self.__y = y
 
     def add(self) -> float:
         return self.__x + self.__y
+
+    def substract(self) -> float:
+        return self.__x - self.__y
